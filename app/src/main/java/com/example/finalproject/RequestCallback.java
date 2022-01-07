b0f7b2a91b38f0718d0e85f2a00e9bd5ba7231aa
omard <omar.dalal@outlook.com>
2022-01-07 21:25:44 +0200

    Added comment to RequestCallback interface

--- app/src/main/java/com/example/finalproject/RequestCallback.java
+++ app/src/main/java/com/example/finalproject/RequestCallback.java
@@ -3,6 +3,9 @@ package com.example.finalproject;
 import org.json.JSONObject;
 import java.util.ArrayList;
 
+/**
+ * Callback used when performing Get request
+ */
 public interface RequestCallback {
     void onResponse(ArrayList<JSONObject> response, boolean success);
 }
